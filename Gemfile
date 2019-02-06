4086af057faa7c6fb89a748b7e67711199dc5419
Marii <mn2802@columbia.edu>
2019-02-06 14:14:11 -0500

    better ui

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'jekyll', '~> 3.8'
 gem 'minima', '~> 2.1.1'
+gem 'wax_annotate', path: 'wax_annotate'
--- _site/assets/annotation_endpoint.js
+++ _site/assets/annotation_endpoint.js
@@ -1,23 +1,39 @@
+// ON LOAD
+
+
 $(document).ready(function() {
   $('#no_anno_button').click(function() {
     $("#anno_results").empty();
   });
 
-  $('#anno_button').click(function() {
-    var keys            = Object.keys(localStorage);
-    var canvases        = keys.filter(function(k){ return k.startsWith('http'); });
-    var sumAnnotations  = [];
+  $('#no_gui_button').click(function() {
+    $("#gui_results").empty();
+  });
 
-    for (c in canvases) {
-      var annotations = JSON.parse(localStorage[canvases[c]]);
-      for (i in annotations) {
-        sumAnnotations.push(annotations[i]);
-      }
-    }
+  $('#anno_button').click(function() {
+    var sumAnnotations = getLocalAnnotations();
 
     $('#anno_results').empty();
-    $('#anno_results').append(`<div class='anno'><h3>Result: ${sumAnnotations.length} locally generated annotations were found</h3><textarea rows='30' cols='100' id='annotationJSON'>${JSON.stringify(sumAnnotations, null, 2)}</textarea></div>`);
+    $('#anno_results').append(`<div class='anno'><h3>Results (JSON): ${sumAnnotations.length} locally generated annotations were found</h3><textarea rows='30' cols='100' id='annotationJSON'>${JSON.stringify(sumAnnotations, null, 2)}</textarea></div>`);
   });
+
+
+  $('#gui_button').click(function() {
+    var sumAnnotations = getLocalAnnotations();
+    $("#gui_results").empty();
+    $('#gui_results').append(`<div class='anno'><h3>Results (GUI):</h3><ul id='annotationGUI'></ul></div>`)
+    for (a in sumAnnotations) {
+      $('#annotationGUI').append(`<li>${annotationToText(sumAnnotations[a])}</li>`);
+    };
+
+    $('.annotation_link').click(function() {
+      var canvas = $(this).attr('canvas');
+      var manifest = $(this).attr('manifest');
+
+      turnPageTo(canvas, manifest);
+    });
+  });
+
 
   $("#download").click(function() {
     var d = new Date();
@@ -27,6 +43,61 @@ $(document).ready(function() {
     download(filename, $('#annotationJSON').val());
   });
 });
+
+
+
+
+
+// FUNCTIONS
+
+
+
+
+function turnPageTo(canvas, manifest) {
+  console.log(window.viewer.canvasID);
+  window.viewer.canvasID = canvas;
+};
+
+function annotationToText(anno) {
+  var text = [];
+  var canvas = anno['on'][0]['full'];
+  var canvas_name = canvas.split('/').slice(-1).pop();
+  var manifest = anno['on'][0]['within']['@id'];
+
+  console.log(anno);
+
+  for (r in anno['resource']) {
+    var resource = anno['resource'][r];
+    if (resource['@type'] == 'dctypes:Text') {
+      text.push(resource['chars'].replace(/(<([^>]+)>)/ig, ''));
+    }
+  }
+
+  text = text.join(', ');
+  if(text.length > 50) {
+    text = `${text.substring(0,49)}...`;
+  }
+
+  return `<b>"${text}"</b> on canvas <a href='#${canvas_name}' canvas='${canvas}' manifest='${manifest}' class='annotation_link'>#${canvas_name}</a>`;
+}
+
+
+
+
+function getLocalAnnotations() {
+  var keys            = Object.keys(localStorage);
+  var canvases        = keys.filter(function(k){ return k.startsWith('http'); });
+  var sumAnnotations  = [];
+
+  for (c in canvases) {
+    var annotations = JSON.parse(localStorage[canvases[c]]);
+    for (i in annotations) {
+      sumAnnotations.push(annotations[i]);
+    }
+  }
+
+  return sumAnnotations;
+}
 
 function download(filename, text) {
   var element = document.createElement('a');
@@ -71,7 +142,8 @@ function createAnnotationAdminViewer(manifests) {
       loadedManifest: manifests[0],
       displayLayout: false,
       bottomPanel: false,
-      sidePanel: false
+      sidePanel: false,
+
     }]
   };
   for (m in manifests) {
